321af4e8047e413d96281bb082e1a25a47dbb47d
Sirious05 <58982535+Sirious05@users.noreply.github.com>
2021-06-30 14:35:38 +0300

    Update timer.js

--- YogaProject/src/js/parts/timer.js
+++ YogaProject/src/js/parts/timer.js
@@ -1,6 +1,6 @@
 // Timer
 function timer(params) {
-    let deadline = '2020-10-03';
+    let deadline = '2022-10-03';
 
     function getTimeRemaining() {
         let all = Date.parse(deadline) - Date.parse(new Date()),
